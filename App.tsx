3d4575d9e566840ea3ae9fc29a1a7470ac3dec5e
Joshua Gamiao <aws.jegamiao@gmail.com>
2024-11-20 15:44:15 +0800

    Auto time stamp component refactored in App.tsx

--- App.tsx
+++ App.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { ScrollView, SafeAreaView, View } from 'react-native';
 import "./global.css";
 import AutoTimestamp from './src/components/time/AutoTimeStamp';
-import TimePicker from './src/components/time/Time';
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ScrollView className="bg-gray-100 p-4">
         <View>
-          <TimePicker />
+          <AutoTimestamp />
         </View>
       </ScrollView>
     </SafeAreaView>
