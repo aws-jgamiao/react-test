10f77de53118c7392f3cfbff1cbe33317fc459a8
Joshua Gamiao <aws.jegamiao@gmail.com>
2024-11-21 13:58:06 +0800

    Added input component initial design setup

--- App.tsx
+++ App.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import { ScrollView, SafeAreaView, StatusBar } from 'react-native';
-import ShiftCard from './src/components/card/ShiftCard';
-import "./global.css"
-import TimePicker from './src/components/time/Time';
+import { ScrollView, SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
+import EditableInput from './src/components/inputs/EditableInput';
+import VirtualInput from './src/components/inputs/VirtualInput';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView className="bg-gray-100 p-4">
-        <TimePicker />
+    <SafeAreaView style={styles.safeArea}>
+      <StatusBar barStyle="dark-content" />
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <View style={styles.section}>
+          <EditableInput label="Input" placeholder="Some string" />
+          <VirtualInput
+            label="Input - Virtual"
+            value="Some string"
+            helperText="Locked by default"
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#f3f4f6',
+  },
+  scrollView: {
+    padding: 16,
+  },
+  section: {
+    marginBottom: 16,
+  },
+});
