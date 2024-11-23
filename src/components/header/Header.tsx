68d7b7fc927a853ed5fb77f4df26e335dfe852da
Joshua Gamiao <aws.jegamiao@gmail.com>
2024-11-23 15:36:13 +0800

    Updated header components styling to tailwind classes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, SafeAreaView, Platform, StyleSheet, useWindowDimensions } from 'react-native';
+import { View, Image, SafeAreaView, Platform } from 'react-native';
 import Logo from '../icons/Logo';
 
 interface HeaderProps {
@@ -7,63 +7,31 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ userProfileImage }) => {
-  const { height, width } = useWindowDimensions(); // Get the current window size
   const isMobile = Platform.OS === 'ios' || Platform.OS === 'android';
 
-  // Safe area height calculation for iPhone with Dynamic Island
-  const topSafeAreaPadding = isMobile ? 20 : 0; // Adjust based on safe area
-
   return (
-    // If mobile, use SafeAreaView to handle notches/Dynamic Island
     isMobile ? (
-      <SafeAreaView style={[styles.safeAreaContainer, { paddingTop: topSafeAreaPadding }]}>
-        <View style={styles.container}>
-          {/* Left: App Logo */}
+      <SafeAreaView className="flex-1">
+        <View className="flex-row justify-between items-center px-4 h-20 z-10">
           <Logo />
-          
-          {/* Right: User Profile */}
           <Image 
             source={{ uri: userProfileImage }} 
-            style={styles.profileImage}
+            className="w-12 h-12 rounded-full"
             resizeMode="cover"
           />
         </View>
       </SafeAreaView>
     ) : (
-      // On Web, we do not need SafeAreaView but still use the container style
-      <View style={styles.container}>
+      <View className="flex-row justify-between items-center px-4 h-20 z-10">
         <Logo />
         <Image 
           source={{ uri: userProfileImage }} 
-          style={styles.profileImage}
+          className="w-12 h-12 rounded-full"
           resizeMode="cover"
         />
       </View>
     )
   );
 };
-
-// Styles
-const styles = StyleSheet.create({
-  // Safe Area Container
-  safeAreaContainer: {
-    flex: 1,
-  },
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 16, // Horizontal padding for mobile
-    backgroundColor: 'transparent', // No background color
-    height: 80, // Default header height
-    zIndex: 1,
-    // No shadow or border
-  },
-  profileImage: {
-    width: 48, // Profile image size
-    height: 48,
-    borderRadius: 24, // Circular profile picture
-  },
-});
 
 export default Header;
