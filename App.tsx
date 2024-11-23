7f0c51f5efe99f2e1d0d163edc4f257b93f6f929
Joshua Gamiao <aws.jegamiao@gmail.com>
2024-11-23 15:29:05 +0800

    Refactored styling to focus on tailwind classes

--- App.tsx
+++ App.tsx
@@ -1,51 +1,19 @@
 import React from 'react';
-import ShiftCard from './src/components/card/ShiftCard';
+import { View, ScrollView, StatusBar } from 'react-native';
 import BottomNavBar from './src/components/card/BottomNavBar';
+import "./global.css";
 import { TailwindProvider } from 'tailwindcss-react-native';
-import { ScrollView, SafeAreaView, StatusBar, View } from 'react-native';
-import "./global.css"
-import TimePicker from './src/components/time/Time';
 
 export default function App() {
   return (
-    
-      <SafeAreaView style={{ flex: 1 }}>
-        <View className="flex-1 bg-gray-100">
-          
-      
+    <View className="flex-1 bg-gray-100">
       <ScrollView className="p-4">
-      <TimePicker />
-        <ShiftCard
-          status="Uncomplete"
-          duration="3hrs"
-          distance="15km"
-          userName="Donald Duck"
-          date="14 October 2024"
-          time="6am - 9am"
-          location="CC - Newcastle Cluster 1 - Standard Care"
-          profileImage="https://img.freepik.com/free-vector/cute-duck-cartoon-character-holding-mug-beer_1308-118197.jpg?ga=GA1.1.2133803969.1730878438&semt=ais_hybrid"
-          notificationCount={2}
-        />
-        <ShiftCard
-          status="Complete"
-          duration="8hrs"
-          distance="5km"
-          userName="Mickey Mouse Joe"
-          date="15 October 2024"
-          time="9am - 5pm"
-          location="5678 Elm Street, Town"
-          profileImage="https://img.freepik.com/premium-photo/mickey-mouse-with-his-arms-raised-air_971166-158674.jpg?ga=GA1.1.2133803969.1730878438&semt=ais_hybrid"
-          notificationCount={0}
-        />
-        <StatusBar />
       </ScrollView>
       
       {/* Bottom Navigation Bar */}
-      <TailwindProvider platform="web">
+      <TailwindProvider platform="native">
         <BottomNavBar />
       </TailwindProvider>
-      </View>
-      </SafeAreaView>
-    
+    </View>
   );
-};
+}
