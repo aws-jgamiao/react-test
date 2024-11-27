1fe8410b0a82abf41c1713ca39999f291635d15c
Joshua Gamiao <aws.jegamiao@gmail.com>
2024-11-27 16:39:18 +0800

    Updated section header component, and packages

--- App.tsx
+++ App.tsx
@@ -4,13 +4,15 @@ import BottomNavBar from './src/components/card/BottomNavBar';
 import './global.css';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import Header from './src/components/header/Header';
+import ShiftSectionHeader from './src/components/sections/ShiftSectionHeader';
 
 export default function App() {
   return (
     <View className="flex-1 bg-gray-500">
         <Header userProfileImage={'https://cdn-icons-png.flaticon.com/512/6858/6858504.png'} />
 
-        <ScrollView className="p-4">
+        <ScrollView className="">
+          <ShiftSectionHeader userName={'Nova'} shiftsCount={3}/>
         </ScrollView>
         
         <TailwindProvider platform="native">
